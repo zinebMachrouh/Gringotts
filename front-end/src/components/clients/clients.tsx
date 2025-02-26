163b2093404556a309176bf1ec8f18841da5e9aa
zinebMachrouh <zinebmac49@gmail.com>
2025-02-26 13:24:32 +0000

    add CORS configuration for account and customer services; implement client fetching in front-end

--- front-end/src/components/clients/clients.tsx
+++ front-end/src/components/clients/clients.tsx
@@ -1,7 +1,34 @@
+import { useEffect, useState } from "react";
+import { Client } from "./client";
+import axios from "axios";
+
 const Clients = () => {
+    const [clients,setClients] = useState<Client[]>([]);
+
+    const getClients = async () => {
+        try {
+            const response = await axios.get('http://localhost:9090/customers', {
+                headers: { 'Content-Type': 'application/json' }
+            });
+            setClients(response.data);
+        } catch (error) {
+            console.error("Error fetching clients:", error);
+        }
+    };
+    
+
+    useEffect(() => {
+        getClients();
+    },[]);
+
     return (  
         <>
             <h1>Clients</h1>
+            <ul>
+                {clients.map((client) => (
+                    <li key={client.id}>{client.name} - {client.email}</li>
+                ))}
+            </ul>
         </>
     );
 }
