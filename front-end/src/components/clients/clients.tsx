11052c704857202de75dec465b79970a9c9f3ebf
zinebMachrouh <zinebmac49@gmail.com>
2025-02-27 09:52:54 +0000

    refactor layout structure; enhance client management with search and add functionality; update styles and integrate Font Awesome

--- front-end/src/components/clients/clients.tsx
+++ front-end/src/components/clients/clients.tsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react";
 import { Client } from "./client";
+import ClientCard from "./clientCard";
 import axios from "axios";
 
 const Clients = () => {
@@ -15,6 +16,41 @@ const Clients = () => {
             console.error("Error fetching clients:", error);
         }
     };
+
+    const addClient = async (client: Client) => {
+        try {
+            const response = await axios.post('http://localhost:9090/customers', client, {
+                headers: { 'Content-Type': 'application/json' }
+            });
+            setClients([...clients, response.data]);
+        } catch (error) {
+            console.error("Error adding client:", error);
+        }
+    };
+
+    const searchClients = async (search: string) => {
+       if(search.trim() === "") {
+           getClients();
+           return;
+       }else {
+            if(search.includes('-')){
+                try {
+                    const response = await axios.get(`http://localhost:9090/customers/${search}`, {
+                        headers: { 'Content-Type': 'application/json' }
+                    });
+                    setClients(response.data);
+                } catch (error) {
+                    console.error("Error fetching clients:", error);
+                }
+            }else{
+                const searchClients = clients.filter((client) => {
+                    return client.name.toLowerCase().includes(search.toLowerCase()) || client.email.toLowerCase().includes(search.toLowerCase());
+                });
+                setClients(searchClients);
+            }
+        }
+    };
+
     
 
     useEffect(() => {
@@ -23,12 +59,24 @@ const Clients = () => {
 
     return (  
         <>
-            <h1>Clients</h1>
-            <ul>
-                {clients.map((client) => (
-                    <li key={client.id}>{client.name} - {client.email}</li>
-                ))}
-            </ul>
+            <div className="clients-header">
+                <h1>Clients</h1>
+
+                <div className="actions">
+                    <div className="search">
+                        <input type="text" placeholder="Search clients" onChange={(e)=> searchClients(e.target.value)}/>
+                        <button type="button" title="search"><i className="fa-solid fa-magnifying-glass"></i></button>
+                    </div>
+                    <button type="button" title="Add client" className="addClient"><i className="fa-solid fa-plus"></i></button>
+                </div>
+            </div>
+            <div className="clients">
+                {
+                    clients.length > 0 ? clients.map((client) => {
+                        return <ClientCard key={client.id} client={client} />
+                    }) : <p>No clients found</p>
+                }
+            </div>
         </>
     );
 }
--- front-end/src/components/layout.tsx
+++ front-end/src/components/layout.tsx
@@ -2,9 +2,9 @@ import { Outlet } from "react-router-dom";
 
 const Layout = () => {
     return (  
-        <>
+        <section className="layout">
             <Outlet />
-        </>
+        </section>
     );
 }
  
